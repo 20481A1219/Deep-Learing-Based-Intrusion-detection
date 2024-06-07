e3c874ae6a5d673d838333b8842d8c4ccba8f933
Boppa Sri Satya Sai Hruday <81548274+20481A1219@users.noreply.github.com>
2024-06-07 15:32:29 +0530

    Update message.py

--- 2. Message and Mail codes/message.py
+++ 2. Message and Mail codes/message.py
@@ -1,8 +1,8 @@
 import twilio.rest
 print("Sending Message")
-client = twilio.rest.Client("ACf23bb0e70ce0993029fd32f642ba2383", "2d4131c30869fbefeefd0bca5bb2b960")
-from_ = "+16317106743"
-to = "+918143266869"
+client = twilio.rest.Client("xxx", "xxxx")
+from_ = "+xxxx"
+to = "+xxxx"
 message = "Your RaspberryPi is found in intrusion."
 message = client.messages.create(
 	to=to,
